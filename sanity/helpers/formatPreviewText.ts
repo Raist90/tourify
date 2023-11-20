862d561396fa88dd682ae7ef9774afc329ebd3a9
Jaromil90 <manuel.detoma@noaignite.com>
2023-11-20 20:50:32 +0100

    fix: change formatPreviewText returned value

--- sanity/helpers/formatPreviewText.ts
+++ sanity/helpers/formatPreviewText.ts
@@ -3,8 +3,9 @@ import type { TypedObject } from "sanity"
 /** @todo Make sure to properly type this */
 export const formatPreviewText = (text: TypedObject[]) => {
   if (!text) return 'Missing text'
+
   return text
     .filter((block: Record<string, any>) => block._type === 'block')
     .map((block: Record<string, any>) => block.children.map((child: Record<string, any>) => child.text).join(''))
-    .join('\n') || 'Missing title or text'
+    .join('\n') || 'Missing text'
 }
