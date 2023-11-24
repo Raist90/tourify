cca3ef5d2bc22b157525dd7f62db625143bddae1
Jaromil90 <manuel.detoma@noaignite.com>
2023-11-24 18:35:54 +0100

    delete a console.log

--- src/app/blocks/Text.tsx
+++ src/app/blocks/Text.tsx
@@ -1,16 +1,17 @@
-import { ComponentType } from "react"
-import { PortableText } from "@portabletext/react"
+import { ComponentType } from 'react'
+import { PortableText } from '@portabletext/react'
 import type { TextBlock } from '@/types/blocks'
 
-export const Text: ComponentType<TextBlock> = ({ renderTitle, title, text, isTextSticky = false }) => {
+export const Text: ComponentType<TextBlock> = ({
+  renderTitle,
+  title,
+  text,
+  isTextSticky = false,
+}) => {
   const sticky = isTextSticky ? 'md:sticky md:place-self-end md:bottom-12' : ''
-  console.log('isTextSticky', isTextSticky)
   return (
     <section className={`flex flex-col gap-2 ${sticky}`}>
-      {renderTitle && (
-        <h2 className="text-2xl">{title}</h2>
-      )
-      }
+      {renderTitle && <h2 className='text-2xl'>{title}</h2>}
       <PortableText value={text} />
     </section>
   )
