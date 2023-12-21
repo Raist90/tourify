3675bd528dfa8543aa37b01b4b634de485106f86
Jaromil90 <manuel.detoma@noaignite.com>
2023-12-21 14:08:48 +0100

    add query value as header and text for search routes

--- src/helpers/serverHelpers/pageGetter.tsx
+++ src/helpers/serverHelpers/pageGetter.tsx
@@ -6,7 +6,10 @@ export const pageGetter = (route: string, opts: string) => {
 
   if (pageTypeList.includes(route)) {
     const keyword = opts
-    const header = { title: 'Test', text: 'Test' }
+    const header = {
+      title: opts || 'all',
+      text: `All available results for the query "${opts || 'all'}"`,
+    }
     return <Feed header={header} keyword={keyword} />
   }
 }
