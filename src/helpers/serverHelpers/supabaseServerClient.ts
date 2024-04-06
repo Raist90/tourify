fe5800d44d46fde0da2ffaa34db72a9b9290aeb8
Jaromil90 <manuel.detoma@noaignite.com>
2024-04-06 14:10:20 +0200

    fix annoying supabase cookie problem and pageFieldsQuery

--- src/helpers/serverHelpers/supabaseServerClient.ts
+++ src/helpers/serverHelpers/supabaseServerClient.ts
@@ -3,18 +3,18 @@ import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createClient() {
-  const cookieStore = cookies()
-
   return createServerClient(
     CLIENT_ENV.NEXT_PUBLIC_SUPABASE_URL,
     CLIENT_ENV.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name: string) {
+          const cookieStore = cookies()
           return cookieStore.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
           try {
+            const cookieStore = cookies()
             cookieStore.set({ name, value, ...options })
           } catch (error) {
             // The `set` method was called from a Server Component.
@@ -24,6 +24,7 @@ export function createClient() {
         },
         remove(name: string, options: CookieOptions) {
           try {
+            const cookieStore = cookies()
             cookieStore.set({ name, value: '', ...options })
           } catch (error) {
             // The `delete` method was called from a Server Component.
